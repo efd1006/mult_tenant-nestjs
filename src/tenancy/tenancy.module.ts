a6d1d4ad175d6a73b7d2dfbd5c5afa384a2ae9f3
IanTorquato <ian@silbeck.com.br>
2021-03-12 14:11:25 -0300

    Add User module + Install mysql2

--- src/tenancy/tenancy.module.ts
+++ src/tenancy/tenancy.module.ts
@@ -12,10 +12,14 @@ import { NextFunction, Request, Response } from 'express';
 import { TenancyService } from './tenancy.service';
 import { TenancyController } from './tenancy.controller';
 import { Tenancy } from './tenancy.entity';
+import { User } from 'src/user/user.entity';
+import { TenancyProvider } from './tenancy.provider';
 
 @Module({
+  // Acesso ao database
   imports: [TypeOrmModule.forFeature([Tenancy])],
-  providers: [TenancyService],
+  providers: [TenancyService, TenancyProvider],
+  exports: [TenancyProvider],
   controllers: [TenancyController],
 })
 export class TenancyModule {
@@ -27,11 +31,13 @@ export class TenancyModule {
     // Empty
   }
 
+  // Todas as classes de modulos possuem o método "configure"
+  // onde se configura os middleware necessários
   configure(consumer: MiddlewareConsumer): void {
     consumer
       .apply(
         async (request: Request, response: Response, next: NextFunction) => {
-          // silbeck.com/tenancyName/api
+          // silbeck.com/tenancyHotelName/api
           const name: string = request.params['tenancy'];
           const tenancy: Tenancy = await this.tenancyService.findOne(name);
 
@@ -46,20 +52,21 @@ export class TenancyModule {
             getConnection(tenancy.name);
             next();
           } catch (error) {
+            // Not supported in Postgres
             await this.connection.query(
               `CREATE DATABASE IF NOT EXISTS ${tenancy.name}`,
             );
 
             const createdConnection: Connection = await createConnection({
               name: tenancy.name,
-              type: 'postgres',
+              type: 'mysql',
               host: this.configService.get('DB_HOST'),
               username: this.configService.get('DB_USER'),
               password: this.configService.get('DB_PASSWORD'),
               port: this.configService.get('DB_PORT'),
               database: tenancy.name,
               synchronize: true,
-              entities: [], // Add User entity
+              entities: [User],
             });
 
             if (createdConnection) {
